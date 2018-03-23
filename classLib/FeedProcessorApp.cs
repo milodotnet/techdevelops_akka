250f9c98546159375548cb1f10b7f3bc30cd0004
Sasa Milovic <sasa.milovic@asos.com>
2018-03-23 11:43:19 +0000

    add partition range actor

--- classLib/FeedProcessorApp.cs
+++ classLib/FeedProcessorApp.cs
@@ -5,16 +5,51 @@ using Akka.Event;
 namespace classLib
 {
 
+    public class StartReadingPartitions {
+
+    }
+
+    public class PartitionDetected {
+        public string Id {get;set;}
+
+        public override string ToString(){
+            return Id;
+        }
+    }
+
+    public class PartitionRangeActor : ReceiveActor
+    {
+        public ILoggingAdapter Log { get; } = Context.GetLogger();
+    
+        public PartitionRangeActor()
+        {
+            Receive<StartReadingPartitions>(message => {
+                            //start polling to see which partitions exits, and when i find one, send the partition detected message
+               Context.Parent.Tell(new PartitionDetected {  Id = "1" });
+               Log.Info("Start processing");
+            });
+        }
+    }
+
     public class CollectionSupervisor : UntypedActor
     {
         public ILoggingAdapter Log { get; } = Context.GetLogger();
 
-        protected override void PreStart() => Log.Info("Feed Processor Application started");
-        protected override void PostStop() => Log.Info("Feed Processor Application stopped");
+        protected override void PreStart() {
+            IActorRef actorRef = Context.ActorOf<PartitionRangeActor>("partition-range");
+            actorRef.Tell(new StartReadingPartitions());
+            Log.Info("Feed Processor Application started"); 
+        }
 
+        protected override void PostStop() => Log.Info("Feed Processor Application stopped");
+        
+    
         // No need to handle any messages
         protected override void OnReceive(object message)
         {
+            if(message is PartitionDetected){
+                Log.Info($"Partition detected! {message}");
+            }
         }
 
         public static Props Props() => Akka.Actor.Props.Create<CollectionSupervisor>();
@@ -27,7 +62,7 @@ namespace classLib
             using (var system = ActorSystem.Create("feedprocessor-system"))
             {
                 // Create top level supervisor
-                var supervisor = system.ActorOf(Props.Create<CollectionSupervisor>(), "collection-supervisor");
+                var supervisor = system.ActorOf(CollectionSupervisor.Props(), "collection-supervisor");
                 // Exit the system after ENTER is pressed
                 Console.ReadLine();
             }
